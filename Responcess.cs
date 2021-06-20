3ac5ef8b62e6f14ae15fba4ded9aedc35eba4eb0
santoshMachhindrakharde <85839142+santoshMachhindrakharde@users.noreply.github.com>
2021-06-20 12:35:52 +0530

    change
    submit

---
+++ Responcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTestSecond
+{
+    public class Responcess<T>
+    {
+        public int ErrorId { get; set; }
+        public int ErrorCode { get; set; }
+        public List<string> ErrorMessage { get; set; }
+        public T Data { get; set; }
+    }
+        public class SqlResponce<T>
+        {
+        public int id { get; set; }
+
+        public T fulldata { get; set; }
+
+        public string Errormsg { get; set; }
+    }
+
+
+    
+}
